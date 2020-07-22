bf79794e37e1ee588d5fa4a776e62f6ee01d80ac
xhasu <ing.xhasu@gmail.com>
2020-07-22 10:57:44 -0500

    add animation group and actions

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@
 
     var swiper = new Swiper('.slide .swiper-container', {
         slidesPerView: 'auto',
+        slidesPerGroup: 1,
+        speed: 600,
         spaceBetween: 20,
         loop: true,
         breakpoints: {
             1280: {
                 slidesPerView: 4,
+                slidesPerGroup: 2,
             },
+        },
+        navigation: {
+            prevEl: '.slide-actions .slide-prev',
+            nextEl: '.slide-actions .slide-next',
         },
     });
 
